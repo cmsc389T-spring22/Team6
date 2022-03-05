03fb9f51100c42324db77417503ede17a5871824
kianafk <58780852+kianafk@users.noreply.github.com>
2022-03-04 22:26:29 -0500

    Update TestGhostMove.java

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -2,9 +2,20 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
 	
+		NoFrame frame = new NoFrame(); 
+		Location loc1 = new Location(1,1);
+		Ghost ghost = frame.addGhost(loc1,"Blinky",Color.red);
+
+		assertTrue(frame.getMap().getLoc(loc1).contains(Map.Type.GHOST));
+		assertTrue(pacman.move());
+		assertTrue(!(frame.getMap().getLoc(loc1).contains(Map.Type.GHOST)));
 	}
 }
