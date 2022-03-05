dcb1f9f22e1de30fab4659f0d1841047a918daa7
matnafu2 <micomike30@gmail.com>
2022-03-04 22:31:28 -0500

    add test for is_ghost_in_range

--- Projects/P3/tests/TestGhostInRange.java
+++ Projects/P3/tests/TestGhostInRange.java
@@ -4,8 +4,24 @@ import java.io.*;
 
 public class TestGhostInRange extends TestCase {
 
+	// should be in range
 	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+		MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(3, 2));
+		Ghost ghost= frame.addGhost(new Location(3, 1), "testLeft", Color.red);
+
+
+		assertTrue(pacman.is_ghost_in_range());
+	}
+
+	should not be in range
+	public void testGhostInRange2() throws FileNotFoundException{
+		MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(3, 2));
+		Ghost ghost= frame.addGhost(new Location(4, 5), "testLeft", Color.red);
+
+
+		assertEquals(pacman.is_ghost_in_range(), false);
 	}
 }
 
